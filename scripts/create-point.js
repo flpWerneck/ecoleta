76c19221d6bbef3c5f939d2dc7136139179326e6
flpWerneck <felipewerneck58@gmail.com>
2020-06-05 00:33:20 -0300

    Aula 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,4 +1,4 @@
-function fetchIBGE(url) {
+function fetchIbge(url) {
     return fetch(url)
         .then(res => res.json())
 }
@@ -8,7 +8,7 @@ function PopulateUFs() {
     const ufSelect = document.querySelector("select[name=uf]");
     const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
-    fetchIBGE(url)
+    fetchIbge(url)
         .then(ufs => {
             ufs.forEach(uf => {
                 ufSelect.innerHTML += `<option value=${uf.id}>${uf.nome}</option>`
@@ -25,13 +25,15 @@ function getCities(event) {
 
     const ufValue = event.target.value;
 
+    const indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
     citySelect.innerHTML = `<option value = "">Selecione a Cidade</option>`
     citySelect.disabled = true
 
-    fetchIBGE(url)
+    fetchIbge(url)
         .then(cities => {
 
             cities.forEach(city => {
@@ -44,3 +46,40 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+// Itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+itemsToCollect.forEach(item => {
+    item.addEventListener("click", handleSelectedItem)
+});
+
+
+let selectedItems = []
+
+const collectedItems = document.querySelector("input[name=items]")
+
+function handleSelectedItem(event) {
+
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //checa se o item dentro do array selectedItems é igual ao itemLi (clicado no site),
+    const alreadySelected = selectedItems.findIndex(item => item == itemId) //-1 se não encontrar, [indice] se encontrar
+
+
+    //verifica se está selecionado e tira do array
+    if (alreadySelected != -1) {
+        const filteredItems = selectedItems.filter(item => item != itemId) //se der false exclui o itemId do array.
+        selectedItems = filteredItems
+    } else selectedItems.push(itemId) //add no array
+
+    console.log(selectedItems)
+
+    collectedItems.value = selectedItems
+
+}
